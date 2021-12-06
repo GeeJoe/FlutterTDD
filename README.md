17b71841938b2475986f496c5d3f9b1ff1e3b293
zhiyueli <zhiyueli@tencent.com>
2021-12-06 20:48:48 +0800

    feat: update coverage

---
+++ README.md
@@ -0,0 +1,5 @@
+# Flutter TDD
+
+![coverage](./coverage_badge.svg)
+
+[《我的 Flutter TDD 心路历程》](我的 Flutter TDD 心路历程.md)
--- test_coverage.sh
+++ test_coverage.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-source coverage_helper.sh mirai
+source coverage_helper.sh flutter_tdd
 flutter test --coverage
-flutter pub run remove_from_coverage -f coverage/lcov.info -r 'lib/model/proto/*'
+flutter pub run remove_from_coverage -f coverage/lcov.info
 flutter pub run test_coverage_badge
 git add coverage_badge.svg
 git commit -m "Update coverage badge"
